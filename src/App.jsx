78b6d89d17aa5a3126d78cc5a1b08677643014ee
= <=>
2024-03-28 18:32:06 -0300

    subindo parte da funcao de exclusao de comentario

--- src/App.jsx
+++ src/App.jsx
@@ -55,11 +55,12 @@ export function App() {
          {posts.map(post => {
           return (
             <Post
+              key={post.id}
               author={post.author}
               content={post.content}
               publishedAt={post.publishedAt}
             />
-          )
+          ) 
          })}
         </main>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
 import styles from './Post.module.css';
-import  { Comment }  from './Comment';
+import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import { useState } from 'react';
 
-export function Post( { author, publishedAt, content } ){
+export function Post({ author, publishedAt, content }) {
 
     const [newCommentText, setNewCommentText] = useState('');
 
@@ -15,26 +15,31 @@ export function Post( { author, publishedAt, content } ){
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
-    }) 
+    })
 
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
         locale: ptBR,
         addSuffix: true,
     })
-    
-    function handleCreateNewComment(){
+
+    function handleCreateNewComment() {
         event.preventDefault();
 
-        setComments([...comments, newCommentText ]);
+        setComments([...comments, newCommentText]);
         setNewCommentText('');
 
 
     }
 
-    function handleNewCommentChange(){
+    function handleNewCommentChange() {
         setNewCommentText(event.target.value);
     }
-    return(
+
+    function deleteComment(comment) {
+        console.log(comment);
+    }
+
+    return (
         <article className={styles.post} >
             <header>
                 <div className={styles.author}>
@@ -46,40 +51,42 @@ export function Post( { author, publishedAt, content } ){
                     </div>
                 </div>
 
-                <time title={publishedDateFormatted}  dateTime={publishedAt.toISOString()}>
-                    {publishedDateRelativeToNow} 
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
                 </time>
             </header>
             <div className={styles.content}>
-               {content.map(line => {
-                    if(line.type === 'paragraph'){
-                        return <p>{line.content}</p>;
-                    }else if(line.type === 'link'){
-                        return <p><a href="#" >{line.content}</a></p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content} >{line.content}</p>;
+                    } else if (line.type === 'link') {
+                        return <p key={line.content} ><a href="#" >{line.content}</a></p>
 
                     }
-               })}
+                })}
             </div>
 
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea 
-                name="comment"
-                placeholder="Deixe seu comentário"
-                value={newCommentText}
-                onChange={handleNewCommentChange}
+                <textarea
+                    name="comment"
+                    placeholder="Deixe seu comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
-                
+
             </form>
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment 
+                    return <Comment
+                        key={comment}
                         content={comment}
+                        onDeleteComment={deleteComment}
                     />
                 })}
             </div>
